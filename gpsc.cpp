5b43bcc0b8d1de02b1ba47525a7b91d1c08c8e85
AbyanM <gxabyan17@gmail.com>
2022-08-10 03:57:41 +0700

    commit gps

---
+++ gpsc.cpp
@@ -0,0 +1,29 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+static const int RXPin = 0, TXPin = 1;
+static const uint32_t GPSBaud = 9600;
+
+TinyGPSPlus gps;
+
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup() {
+ Serial.begin(9600);
+ ss.begin(GPSBaud);
+}
+
+void loop() {
+ delay (1000);
+ while (ss.available() > 0)
+ gps.encode(ss.read());
+
+ if (gps.location.isValid()){
+ double latitude=gps.location.lat();
+ Serial.print(latitude,6);
+ Serial.print(F("|"));
+ Serial.println(gps.location.lng(), 6);
+}
+
+delay (100); 
+}
